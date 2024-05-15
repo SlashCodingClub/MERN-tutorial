56ec66a19b630f7239217073d7a8757349e8356f
OwlinLight <42697056+OwlinLight@users.noreply.github.com>
2024-05-14 20:33:59 -0400

    add jwt

--- backend/models/goalModel.js
+++ backend/models/goalModel.js
@@ -2,6 +2,11 @@ const { Timestamp } = require('mongodb');
 const mongoose = require('mongoose');
 
 const goalSchema = mongoose.Schema({
+    user: {
+        type: mongoose.Schema.Types.ObjectId,
+        required: true,
+        ref: 'User'
+    },
     text: {
         type: String,
         required: [true, 'Pleass add a text value']
